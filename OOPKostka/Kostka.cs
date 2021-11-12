6f46258f087c5170b5a48276087e434d3c665efd
Sardriel <91335030+Sardriel@users.noreply.github.com>
2021-11-12 16:03:16 +0100

    Add project files.

---
+++ OOPKostka/Kostka.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPKostka
+{
+
+    class Kostka
+    {
+        private Random random;
+        private int pocetSten;
+        
+        public Kostka()
+        {
+            
+            pocetSten = 6;
+            random = new Random();
+        }
+        public int hod()
+        {
+            return random.Next(1, pocetSten + 1);
+        }
+       
+    }
+}
---
+++ OOPKostka/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OOPKostka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kostka kostkaJedna = new Kostka();
+            Kostka kostkaDva = new Kostka();
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Na první kostce padlo {0}", kostkaJedna.hod());
+                Console.WriteLine("Na druhé kostce padlo {0}", kostkaDva.hod());
+            }
+            
+
+        }
+    }
+}
